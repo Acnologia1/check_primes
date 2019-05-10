7123ec12c3ed378951faac0f1fc536d3fd977054
vancong <tavancong1994@gmail.com>
2019-05-10 09:26:01 +0700

    Create check_primes.java
    check_primes

---
+++ check_primes.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.Scanner;
+
+public class check_primes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+        if (number < 2)
+            System.out.println(number+" is not a prime");
+        else
+        {
+            int i = 2;
+            boolean check = true;
+
+            while(i <Math.sqrt(number))
+            {
+                if(number == 4)
+                {
+                    check = false;
+                }
+                if(number % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if(check)
+                System.out.println(number+ " is a prime");
+            else
+                System.out.println(number+ " is not a prime");
+        }
+    }
+}
+
